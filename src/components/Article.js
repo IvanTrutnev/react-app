28705bc9311a6b80632bf3eebc21daca61b48b66
Ivan Trutnev <i@gmail.com>
2018-01-10 15:19:28 +0300

    Add refs to components

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
 import CommentsList from './CommentsList';
 import toggleOpen from '../decorators/toggleOpen'
@@ -12,16 +13,33 @@ class Article extends Component {
         }).isRequired
     }
 
+    componentWillReceiveProps(nextProps){
+      console.log('update', this.props.isOpen, nextProps.isOpen);
+    }
+
+    componentWillMount() {
+      console.log('will mount');
+    }
+
     render() {
         const {article, isOpen, toggleOpen} = this.props;
 
         return (
-          <div>
+          <div ref = {this.setContainerRef}>
             <h2>{article.title}</h2>
             <button onClick={toggleOpen}>{isOpen ? 'close': 'open'}</button>
             {this.getBody()}
           </div>
         )
+    }
+
+    setContainerRef = ref => {
+      this.container = ref;
+      console.log(ref);
+    }
+
+    componentDidMount() {
+      console.log('did mount');
     }
 
     getBody() {
@@ -31,9 +49,13 @@ class Article extends Component {
         } else {
           return <section>
             {article.text}
-            <CommentsList comments={article.comments}/>
+            <CommentsList comments={article.comments} ref = {this.setCommentsRef}/>
           </section>
        }
+    }
+
+    setCommentsRef = ref => {
+      console.log(findDOMNode(ref));
     }
 
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import Articlelist from './components/ArticleList'
+import App from './components/App'
 import {articles} from './fictures'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Articlelist articles={articles}/>, document.getElementById('root'));
+ReactDOM.render(<App articles={articles}/>, document.getElementById('root'));
 registerServiceWorker();
